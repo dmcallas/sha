178859f47db1780009dc2eca852bc1dfecb2b491
Daniel P. McAllaster <dmcallas@gmail.com>
2015-05-18 18:52:06 -0700

    Added more tests for bit manipulation.

--- test/test_runners/bit_manip_test_runner.c
+++ test/test_runners/bit_manip_test_runner.c
@@ -5,4 +5,7 @@ TEST_GROUP_RUNNER(bit_manip)
 {
   RUN_TEST_CASE(bit_manip, rotl32);
   RUN_TEST_CASE(bit_manip, rotr32);
+  RUN_TEST_CASE(bit_manip, rotl64);
+  RUN_TEST_CASE(bit_manip, rotr64);
+  RUN_TEST_CASE(bit_manip, bytes_to_uint32);
 }
